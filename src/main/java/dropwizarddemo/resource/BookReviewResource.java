ffc0d161a5d6b97d1e20dcd3c7c44eb592e6f454
Jesús Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-08-16 16:43:12 -0500

    Respond to get petitions

--- src/main/java/dropwizarddemo/resource/BookReviewResource.java
+++ src/main/java/dropwizarddemo/resource/BookReviewResource.java
@@ -1,4 +1,15 @@
 package dropwizarddemo.resource;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.util.Date;
+
+@Path("/book")
 public class BookReviewResource {
+
+    @GET
+    @Path("/healthCheck")
+    public String healthCheck(){
+        return "Ping received at " + new Date();
+    }
 }
