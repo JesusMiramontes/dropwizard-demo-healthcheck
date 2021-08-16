133422449d85759b1bbe1626b19d9de6a813165b
Jesús Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-08-16 16:58:51 -0500

    Added simple post

--- src/main/java/dropwizarddemo/resource/BookReviewResource.java
+++ src/main/java/dropwizarddemo/resource/BookReviewResource.java
@@ -1,6 +1,7 @@
 package dropwizarddemo.resource;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import java.util.Date;
 
@@ -9,7 +10,13 @@ public class BookReviewResource {
 
     @GET
     @Path("/healthCheck")
-    public String healthCheck(){
+    public String getHealthCheck(){
         return "Ping received at " + new Date();
+    }
+
+    @POST
+    @Path("/healthCheck")
+    public String postHealthCheck(){
+        return "Posted";
     }
 }
