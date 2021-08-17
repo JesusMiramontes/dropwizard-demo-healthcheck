a0fe081bd54b3d8f96f6c3c44ed99f70d2284dc5
Jesús Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-08-17 10:52:15 -0500

    Set the response as JSON

--- src/main/java/dropwizarddemo/resource/BookReviewResource.java
+++ src/main/java/dropwizarddemo/resource/BookReviewResource.java
@@ -3,9 +3,12 @@ package dropwizarddemo.resource;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.util.Date;
 
 @Path("/book")
+@Produces(MediaType.APPLICATION_JSON)
 public class BookReviewResource {
 
     @GET
